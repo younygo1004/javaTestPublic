2a9e035702b31998e211ea5a2660691a77002ace
younygo1004 <younygo1004@naver.com>
2024-02-27 12:31:04 +0900

    super type token 기반 Store 일부 수정

--- rpg_console/rpg2.0.0/src/main/java/rpg/item/store/ItemStore.java
+++ rpg_console/rpg2.0.0/src/main/java/rpg/item/store/ItemStore.java
@@ -8,14 +8,15 @@ import java.util.*;
 
 /**
  * 해당 Class는 원래 List<Item> 객체를 저장하기 위해 정의하였다.
+ * 타입 안전 이종 컨테이너 패턴을 Super Type Token을 사용하여 구현했다.
  * 그러나 해당 클래스는 List를 저장하거나 가져올 때, 반드시 Super Type Token을 전달해야 한다.
  * 현재 user.dto 패키지에 List<Item>을 담는 특화 Class InventoryStore가 있기 때문에
  * 해당 Class와 InventoryStore Class 중 용도에 맞는 것을 신중히 결정하는 것이 좋다.
- * 추후 InventoryStore Class 재정의 혹은 새로운 통합 Store Class를 만들어 사용을 고려하자.
+ * 해당 클래스를 조금 더 개량하거나, 일단은 InventoryStore Class를 사용하자.
  */
 public class ItemStore {
 
-    private final Map<Type, Object> itemStore = new HashMap<>();
+    private final Map<Type, List<? extends Item>> itemStore = new HashMap<>();
 
     public ItemStore() {
 
@@ -29,19 +30,13 @@ public class ItemStore {
 
         @SuppressWarnings("unchecked")
         T itemList = (T) itemStore.get(typeReference.getType());
+
         return itemList;
     }
 
     public List<Item> getAll() {
         List<Item> allItemList = new ArrayList<>();
-        Collection<Object> itemValues = this.itemStore.values();
-
-        itemValues.forEach((element) -> {
-
-            @SuppressWarnings("unchecked")
-            List<? extends Item> itemList = (List<? extends Item>) element;
-            allItemList.addAll(itemList);
-        });
+        this.itemStore.values().forEach(allItemList::addAll);
 
         return allItemList;
     }
